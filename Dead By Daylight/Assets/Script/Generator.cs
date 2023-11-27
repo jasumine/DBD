b87ab9bb66316ede090531eb0486dee157b1a7d0
지원 <92582758+jasumine@users.noreply.github.com>
2023-11-27 17:49:52 +0900

    발전기 구현 중
    생존자랑 충돌 할 경우 수리속도 증가
    살인마와 충돌할 경우 손상 구현중

--- Dead By Daylight/Assets/Script/Generator.cs
+++ Dead By Daylight/Assets/Script/Generator.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float repairValue;
+    public float damageValue;
+
+    public bool isComplete = false;
+    public bool isDamaged = false;
+
+    private void Start()
     {
-        
+        repairValue = 0;
+        damageValue = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(isDamaged)
+        {
+            ReduceRepairValue();
+        }
+    }
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (isComplete == false)
+        {
+            if (collision.gameObject.tag == "Survivor")
+            {
+                // 생존자인 경우 발전기 수리
+                if (Input.GetMouseButton(0))
+                {
+                    isDamaged = false;
+                    repairValue += 30f * Time.deltaTime;
+                    if (repairValue >= 100)
+                    {
+                        isComplete = true;
+                    }
+                }
+                
+            }
+
+            if (collision.gameObject.tag == "Killer")
+            {
+                // 살인마인 경우 발전기 손상
+                if (Input.GetKey(KeyCode.Space) && !isDamaged)
+                {
+                    damageValue += 5f * Time.deltaTime;
+                    if (damageValue >= 2)
+                    {
+                        isDamaged = true;
+                    }
+                    else
+                    {
+                        damageValue = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    private void ReduceRepairValue()
+    {
+        repairValue -= 0.1f * Time.deltaTime;
     }
 }
