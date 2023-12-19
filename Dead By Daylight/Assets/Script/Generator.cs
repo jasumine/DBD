73d6641a905a68df67b379f088c7e410ec2dfab6
지원 <92582758+jasumine@users.noreply.github.com>
2023-12-19 17:51:16 +0900

    발전기 스킬체크 구현
    화살표 회전, space 멈춤, 범위 체크 후 성공 및 실패 확인

--- Dead By Daylight/Assets/Script/Generator.cs
+++ Dead By Daylight/Assets/Script/Generator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,10 +14,12 @@ public class Generator : MonoBehaviour
 
     private bool isSkillCheck = false;
     private bool isArrowRotate = false;
+    private bool isSkillSuccess = true;
     public GameObject SkillCheckUI;
-    public GameObject CircleUI;
+    public RectTransform CircleUI;
     public RectTransform ArrowUI;
 
+    float bigSucessMin, bigSucessMax, successMax;
 
     private void Start()
     {
@@ -31,19 +34,95 @@ public class Generator : MonoBehaviour
             ReduceRepairValue();
         }
 
-        if (Input.GetKeyDown("Space"))
-        {
-            Debug.Log("space");
-            isArrowRotate = false;
-        }
-
         if (isArrowRotate)
         {
-            if (ArrowUI.rotation.z >= -360)
+            // 화살 회전
+            if (ArrowUI.rotation.z >= -180)
             {
                 ArrowUI.Rotate(0f, 0f, -4f * Time.deltaTime * 100);
             }
+
+            // space를 이용한 화살 멈춤
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("space");
+                isArrowRotate = false;
+                SkillSuccessRange();
+            }
         }
+
+        if (Input.GetMouseButtonUp(0) && isSkillCheck == true)
+        {
+            // 스킬체크가 떴는데, 마우스를 뗐다면 실패로 처리하기
+            Debug.Log("마우스 떼지마!");
+
+            StartCoroutine(SkillFail());
+
+            isArrowRotate = false;
+            SkillCheckUI.SetActive(false);
+            isSkillCheck = false;
+
+        }
+
+    }
+
+    private void SkillSuccessRange()
+    {
+        Quaternion circleRotation = CircleUI.rotation;
+        Vector3 circleEulerRotation = circleRotation.eulerAngles;
+
+        float target = circleEulerRotation.z;
+
+        Quaternion arrowRotation = ArrowUI.rotation;
+        Vector3 arrowEulerRotation = arrowRotation.eulerAngles;
+
+        float arrow = arrowEulerRotation.z;
+
+        if (target > 0) // 양수 일 때
+        {
+            bigSucessMin = target + 29;
+            bigSucessMax = target + 19;
+            successMax = target - 24;
+        }
+
+        // arrow 범위 체크
+        if(bigSucessMax <= arrow && arrow <= bigSucessMin)
+        {
+            // 대성공
+            Debug.Log("대성공");
+            isSkillSuccess = true;
+            repairValue += 5;
+        }
+        else if (successMax <= arrow && arrow <= bigSucessMax)
+        {
+            // 성공
+            Debug.Log("성공");
+            isSkillSuccess = true;
+        }
+        else
+        {
+            // 실패
+            Debug.Log("실패");
+            StartCoroutine(SkillFail());
+        }
+
+    }
+
+    IEnumerator SkillFail()
+    {
+        // 살인마에게 알림이 가도록 추가하기
+
+
+        // 수리되지 않도록 false 후 진행도 감소 -> 다시 수리
+        Debug.Log("발전기 손상");
+        isSkillSuccess = false;
+        
+        // 0이하가 될 경우 0으로 초기화
+        repairValue -= 5;
+        if(repairValue <=0) repairValue = 0;
+
+        yield return new WaitForSeconds(2f);
+        isSkillSuccess = true;
     }
 
 
@@ -87,38 +166,52 @@ public class Generator : MonoBehaviour
  
     private void Rapairing()
     {
-        repairValue += 1f * Time.deltaTime;
-
-        float randomValue = Random.Range(0f, 200f);
-        if (randomValue <= 5f && isSkillCheck == false)
+        // 발전기 스킬 체크 실패시 진행을 막기 위해 bool 추가
+        if (isSkillSuccess == true)
         {
-            // 확률로 스킬체크 발생
-            StartCoroutine(SkillCheck());
+            repairValue += 0.5f * Time.deltaTime;
+            float randomValue = Random.Range(0f, 100f);
+            if (randomValue <= 5f && isSkillCheck == false)
+            {
+                StopCoroutine(SkillCheck());
+                // 확률로 스킬체크 발생
+                StartCoroutine(SkillCheck());
+            }
         }
     }
 
     IEnumerator SkillCheck()
     {
         Debug.Log("발전기 스킬체크 발생");
-        isSkillCheck = true;
         // 오디오가 들리고
         yield return new WaitForSeconds(0.3f);
+        isSkillCheck = true;
+
+
 
         // 스킬체크 화면 출력
         SkillCheckUI.SetActive(true);
         float zC = Random.Range(-116, -316);
-        CircleUI.transform.rotation = Quaternion.Euler(0, 0, zC);
+        CircleUI.rotation = Quaternion.Euler(0, 0, zC);
 
         // 화살표 회전
-        ArrowUI.transform.Rotate(0, 0, 0);
+        ArrowUI.rotation = Quaternion.Euler(0, 0, 0);
         isArrowRotate = true;
 
+        // 화살표 회전 멈춤
+        yield return new WaitForSeconds(0.9f);
+        // 회전이 멈출때까지(0.9초가 지날 때 까지) space를 안눌렀다면 fail실행 해준다.
+        if(isArrowRotate)
+        {
+            isArrowRotate = false;
 
+            StartCoroutine(SkillFail());
+        }
 
-        yield return new WaitForSeconds(4f);
+        // 스킬체크 이벤트 종료
+        yield return new WaitForSeconds(1f);
         SkillCheckUI.SetActive(false);
         isSkillCheck = false;
-        isArrowRotate = false;
     }
 
 
